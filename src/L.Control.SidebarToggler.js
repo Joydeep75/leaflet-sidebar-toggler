5b636edee5529a97b2019ccb0387e62623fe7b0f
nippo <n@ippo.io>
2014-04-02 18:43:20 +0200

    Initial commit

---
+++ src/L.Control.SidebarToggler.js
@@ -0,0 +1,44 @@
+L.Control.SidebarToggler = L.Control.extend({
+    options: {
+        position: 'topleft',
+        title: {
+            false: 'View informations',
+            true: 'Hide informations'
+        }
+    },
+
+    initialize: function (sidebar, options) {
+        L.setOptions(this, options);
+        this.sidebar = sidebar;
+    },
+
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'leaflet-control-sidebarToggler leaflet-bar leaflet-control');
+
+        this.link = L.DomUtil.create('a', 'leaflet-control-sidebarToggler-button leaflet-bar-part', container);
+        this.link.href = '#';
+
+        this._map = map;
+        this.sidebar.on('show', this._toggleTitle, this);
+        this.sidebar.on('hide', this._toggleTitle, this);
+        this._toggleTitle();
+
+        L.DomEvent.on(this.link, 'click', this._click, this);
+
+        return container;
+    },
+
+    _click: function (e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        this.sidebar.toggle();
+    },
+
+    _toggleTitle: function() {
+        this.link.title = this.options.title[this.sidebar.isVisible()];
+    }
+});
+
+L.control.sidebarToggler= function (options) {
+    return new L.Control.SidebarToggler(options);
+};
